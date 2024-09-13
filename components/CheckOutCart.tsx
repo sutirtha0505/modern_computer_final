dc428a9a21c88e963bab0dfb93d4b41cb7749ecc
sutirtha0505 <tirtharajkarmakarinlinux@gmail.com>
2024-09-13 13:42:35 +0530

    Update

--- components/CheckOutCart.tsx
+++ components/CheckOutCart.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import HeaderCart from './HeaderCart'
 import CustomerDetails from './CustomerDetails'
+import { supabase } from '@/lib/supabaseClient'
 
 const CheckOutCart = () => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+        setUser(user);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUserData();
+  }, []);
+
   return (
     <div className='w-full h-full flex flex-col justify-center items-center gap-6'>
       <HeaderCart />
